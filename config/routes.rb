ba1a9bfe6abcbccccd6d3dd30df7d17fa55ab38b
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-05-20 23:45:00 +0000

    課題ボツ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
+  root to: "home#top"
+  get '/homes/about', to: 'homes#about'
 end
